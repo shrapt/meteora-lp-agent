ea876fd566643873fc4ab1aecbe5e00cd28867e1
shrapt <99196115+shrapt@users.noreply.github.com>
2026-03-25 05:46:39 +0800

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
 set LLM_BASE_URL=https://api.anthropic.com
-set LLM_API_KEY=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
+set LLM_API_KEY=
 set LLM_MODEL=claude-haiku-4-5-20251001
 python agent_loop.py
